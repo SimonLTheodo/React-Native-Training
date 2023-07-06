43ad49fdf2e7c6cb67104073dcb5f99bc963c0d2
Simon Lake <simonl@theodo.co.uk>
2023-07-06 11:12:17 +0100

    feat: setup basic nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,52 @@
-import { ReactElement } from 'react'
-import { StyleSheet, Text } from 'react-native'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack'
+import * as React from 'react'
+import { Button, Text, View } from 'react-native'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    marginBottom: 8
-  },
-  emoji: {
-    fontSize: 82,
-    marginBottom: 24
-  }
-})
+type RootStackParamList = {
+  Home: undefined
+  Details: { number: string }
+}
 
-export default function App(): ReactElement {
+function HomeScreen({ navigation }: NativeStackScreenProps<RootStackParamList, 'Home'>) {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.emoji}>😻</Text>
-        <Text style={styles.text}>Open src/App.tsx to start working on your app!</Text>
-        <Text>Happy hacking! 🙌🏻</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details 1"
+        onPress={() => navigation.navigate('Details', { number: '1' })}
+      />
+      <Button
+        title="Go to Details 2"
+        onPress={() => navigation.navigate('Details', { number: '2' })}
+      />
+    </View>
   )
 }
+function DetailsScreen({
+  route,
+  navigation
+}: NativeStackScreenProps<RootStackParamList, 'Details'>) {
+  const { number } = route.params
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen {number}</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator()
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
